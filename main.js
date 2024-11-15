11c1a2da30750a9a823e740c52ec0cab07ed27a9
Greeshman <bunnymadgamer@gmail.com>
2024-11-15 19:32:38 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+var car =
+{
+    Brand: "Lamborghini" ,
+    EngineType: "V12"
+};
+document.getElementById("output").innerHTML = " The car brand is " + car.Brand + " and " + " with the most powerful engine " + car.EngineType
